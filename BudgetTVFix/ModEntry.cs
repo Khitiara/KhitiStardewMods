29f22d29aa7b07f3f2d0ea4bf6debd6c127d9c7d
Khitiara <robotbrainify@gmail.com>
2024-03-24 14:54:51 -0400

    initial commit

---
+++ BudgetTVFix/ModEntry.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using StardewModdingAPI;
+using StardewValley.Objects;
+
+namespace BudgetTVFix;
+
+[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]
+// ReSharper disable once ClassNeverInstantiated.Global
+public class ModEntry : Mod
+{
+    public override void Entry(IModHelper helper)
+    {
+        Harmony harmony = new(ModManifest.UniqueID);
+        harmony.Patch(original: AccessTools.Method(typeof(TV), nameof(TV.getScreenPosition)),
+            prefix: new HarmonyMethod(typeof(Patches), nameof(Patches.GetScreenPosition_Prefix))
+        );
+    }
+}
---
+++ BudgetTVFix/Patches.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley.Objects;
+
+namespace BudgetTVFix;
+
+internal static class Patches
+{
+    internal static bool GetScreenPosition_Prefix(TV __instance, ref Vector2 __result)
+    {
+        if ("(F)1466".Equals(__instance.QualifiedItemId, StringComparison.InvariantCultureIgnoreCase)) {
+            __result = new Vector2(__instance.boundingBox.X + 24, __instance.boundingBox.Y - 64);
+            return false;
+        }
+
+        return true;
+    }
+}
